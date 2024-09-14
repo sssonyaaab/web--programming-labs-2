e674a0f5de820385c64ae5805da50f7381d0281b
Соня Баранова <sonya.baranova999@gmail.com>
2024-09-15 02:47:05 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Баранова Софья Сергеевна, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Софья Баранова, ФБИ-24, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
