46d9ac060fe7f19ac7bdc724d6a5dac597e3075d
Соня Баранова <sonya.baranova999@gmail.com>
2024-10-11 00:08:04 +0700

    Имя студента

--- app.py
+++ app.py
@@ -201,4 +201,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Соня Баранова'
+    return render_template('example.html', name=name)
