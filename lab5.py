6ccd50d78f0edeeba9741f7d3f12eb087eefec6f
Соня Баранова <sonya.baranova999@gmail.com>
2024-11-22 14:41:54 +0700

    Исправление SQL-инъекции

--- lab5.py
+++ lab5.py
@@ -40,7 +40,7 @@ def login():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -71,14 +71,14 @@ def register():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html',
                                error = "Такой пользователь уже существует")
 
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
     
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
@@ -92,10 +92,10 @@ def list():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
 
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';")
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id, ))
     articles = cur.fetchall()
 
     db_close(conn, cur)
@@ -119,8 +119,8 @@ def create():
     cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
 
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                VALUES ({user_id}, '{title}', '{article_text}');")
+    cur.execute("INSERT INTO articles(user_id, title, article_text) \
+                VALUES (%s, %s, %s);", (user_id, title, article_text))
     
     db_close(conn, cur)
     return redirect('/lab5')
