d9d4ca5f6520cdd2e86f734f8b4c1d85f0c9dbab
Соня Баранова <sonya.baranova999@gmail.com>
2024-12-13 13:58:55 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -72,3 +72,14 @@ def del_film(id):
         return 'Номер выходит за пределы значений', 404
     del films[id]
     return '', 204
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        return 'Номер выходит за пределы значений', 404
+    film = request.get_json()
+    films[id] = film
+    return films[id]
+
+
