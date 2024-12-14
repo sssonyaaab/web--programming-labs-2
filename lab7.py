3c518dc60a2d6d51d7d92b0cb5085f2c78a0cd46
Соня Баранова <sonya.baranova999@gmail.com>
2024-12-14 17:28:00 +0700

    jsonify

--- lab7.py
+++ lab7.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 
 lab7 = Blueprint('lab7', __name__)
 
@@ -57,7 +57,7 @@ films = [
 
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
-    return films
+    return jsonify(films)
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
