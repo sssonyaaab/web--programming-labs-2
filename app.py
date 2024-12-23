461d36db7b241b42a4a27702655f2c70a7fdc082
Соня Баранова <sonya.baranova999@gmail.com>
2024-12-24 00:14:24 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -9,11 +9,30 @@ from lab7 import lab7
 from lab8 import lab8
 from rgz import rgz
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'sonya_baranova_orm'
+    db_user = 'sonya_baranova_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "sonya_baranova_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
