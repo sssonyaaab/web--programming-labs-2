4f22bdb416332030940ad658662bf84f2ba2f602
Соня Баранова <sonya.baranova999@gmail.com>
2024-12-24 01:14:33 +0700

    Модель базы данных

--- db/models.py
+++ db/models.py
@@ -9,7 +9,7 @@ class articles(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     login_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     title = db.Column(db.String(50), nullable = False)
-    article_text = db.Column(db.Boolean)
+    article_text = db.Column(db.Text, nullable = False)
     is_favorite = db.Column(db.Boolean)
     is_public = db.Column(db.Boolean)
     likes = db.Column(db.Integer)
